281221d0bbb9c559b3e74c9812df5f4acf363548
gsaqlaien <gsaqlaien@outlook.com>
2022-03-20 13:06:37 -0700

    first attempt to create a repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadPosts></LoadPosts>
+
     </div>
   );
+}
+
+
+function LoadPosts() {
+
+  const [Posts, setPosts] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+
+  }, [])
+
+  return (
+    <div>
+      <h1>Posts: {Posts.length}</h1>
+
+      {
+        Posts.map(post => <Post title={post.title} body={post.body}></Post>)
+      }
+    </div>
+  )
+}
+
+function Post(props) {
+  return (
+    <div style={{ backgroundColor: "bisque", margin: "30px auto", padding: "12px", width: '25%' }}>
+      <h2>Title: {props.title}</h2>
+      <p>Body: {props.body}</p>
+    </div>
+  )
 }
 
 export default App;
